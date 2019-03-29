78457e77ad361cebb3310aa174b912a37e32151c
悦欣 张 <zhangyuexin121@live.cn>
2019-03-29 16:54:49 +0800

    timed job

--- src/WeatherStation/WeatherStation.Web/Startup.cs
+++ src/WeatherStation/WeatherStation.Web/Startup.cs
@@ -33,6 +33,8 @@ namespace WeatherStation.Web
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddHostedService<TimedJob>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ src/WeatherStation/WeatherStation.Web/TimedJob.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WeatherStation.Web
+{
+    public class TimedJob : IHostedService, IDisposable
+    {
+        private Timer _timer;
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromMinutes(1));
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object state)
+        {
+            
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+
+    }
+}
